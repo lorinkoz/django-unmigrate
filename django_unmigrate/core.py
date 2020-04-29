a41edc6e079ddd6d6a380d8f3a0f4049bd516fc9
Lorenzo Peña <lorinkoz@gmail.com>
2020-04-29 10:15:27 -0400

    Multiple corrections

--- django_unmigrate/core.py
+++ django_unmigrate/core.py
@@ -3,35 +3,73 @@ import sys
 
 from django.conf import settings
 from django.db import connections
-from django.db.migrations.loader import MigrationLoader
 from django.db.migrations.executor import MigrationExecutor
+from django.db.migrations.loader import MigrationLoader
 
 from git import Repo
-
-# TODO: improve migration detection
-# settings.MIGRATION_MODULES = {app_string: module_location_string}
+from git.exc import GitCommandError
 
 
-def get_targets(database, branch="master"):
-    # Getting repo
-    pathname = os.path.dirname(sys.argv[0])
-    repo = Repo(os.path.abspath(pathname), search_parent_directories=True)
-    # Getting diff
-    here = set(repo.git.ls_tree("--name-only", "-r", "HEAD").splitlines())
-    there = set(repo.git.ls_tree("--name-only", "-r", branch).splitlines())
-    migration_files = [x for x in here - there if "migrations/" in x]
-    # Constructing migration targets
-    targets = [(f.split(os.sep)[-3], f.split(os.sep)[-1].split(".")[0]) for f in migration_files]
-    # Preparing executor
+class GitError(Exception):
+    message = ""
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return self.message
+
+
+def get_targets(database, ref="master"):
+    added_targets = get_added_migrations(ref)
+    return get_parents_from_targets(added_targets, database)
+
+
+def get_added_migrations(ref="master"):
+    """
+    Detect the added migrations when compared to ``ref``, and return them as
+    target tuples: (app_name, migration_name)
+    """
+    try:
+        # Getting repo
+        pathname = os.path.dirname(sys.argv[0])
+        repo = Repo(os.path.abspath(pathname), search_parent_directories=True)
+        # Getting diff
+        here = set(repo.git.ls_tree("--name-only", "-r", "HEAD").splitlines())
+        there = set(repo.git.ls_tree("--name-only", "-r", ref).splitlines())
+        # TODO: improve migration detection
+        # settings.MIGRATION_MODULES = {app_name: module_location}
+        # the previous setting allows for customization, other than that, there is
+        # an established default of app_name.migrations
+        migration_files = [x for x in here - there if "migrations/" in x]
+        # Constructing targets
+        return [(f.split(os.sep)[-3], f.split(os.sep)[-1].split(".")[0]) for f in migration_files]
+    except GitCommandError as error:
+        raise GitError(str(error))
+
+
+def get_parents_from_targets(targets, database):
+    """
+    Inspect the migration tree and return the relevant, common parents from the
+    given target tuples: (app_name, migration_name)
+    """
     connection = connections[database]
     connection.prepare_database()
     loader = MigrationLoader(connection)
     executor = MigrationExecutor(connection)
-    # Final move
     plan_dict = {target: set(loader.graph.backwards_plan(target)) for target in targets}
     final_targets = []
+    # Detecting overlapping plans
+    # Since targets could be nodes of the same path, we need to discard the
+    # plans that are subsets of a bigger one
     for target, plan in plan_dict.items():
-        if any([plan & i_plan == plan for i_plan in plan_dict.values() if i_plan != plan]):
+        if any([plan & iter_plan == plan for iter_plan in plan_dict.values() if iter_plan != plan]):
+            # This plan is a subset of an existing plan, so we ignore it
             continue
         final_targets.append(target)
-    return {target: [x.key for x in loader.graph.node_map[target].parents] for target in final_targets}
+    # We only return the parent node of the same app, to avoid unapplying related
+    # migrations from other apps
+    return {
+        target: [x.key for x in loader.graph.node_map[target].parents if x.key[0] == target[0]]
+        for target in final_targets
+    }
--- django_unmigrate/management/commands/unmigrate.py
+++ django_unmigrate/management/commands/unmigrate.py
@@ -3,15 +3,15 @@ from django.core import management
 from django.core.management.base import BaseCommand, CommandError
 from django.db import DEFAULT_DB_ALIAS
 
-from django_unmigrate.core import get_targets
+from django_unmigrate.core import get_targets, GitError
 
 
 class Command(BaseCommand):
-    help = "Unapplies migrations that were added in the current branch, relative to the passed branch."
+    help = "Unapplies migrations that were added in relation to the passed Git ref."
 
     def add_arguments(self, parser):
         parser.add_argument(
-            "branch", nargs="?", default="master", help="Branch to compare existing migrations with.",
+            "ref", nargs="?", default="master", help="Git ref to compare existing migrations.",
         )
         parser.add_argument(
             "--database",
@@ -22,12 +22,24 @@ class Command(BaseCommand):
             "--fake", action="store_true", help="Mark migrations as run without actually running them.",
         )
         parser.add_argument(
+            "--dry-run", action="store_true", help="Just print the target migrations.",
+        )
+        parser.add_argument(
             "--kamikazee", action="store_true", help="Ignore DEBUG=True and run the command anyways.",
         )
 
     def handle(self, *args, **options):
         if not settings.DEBUG and not options["kamikazee"]:
             raise CommandError("Do not run with DEBUG=True, or pass --kamikazee.")
-        for key, targets in get_targets(options["database"], options["branch"]).items():
+        try:
+            targets = get_targets(options["database"], options["ref"]).items()
+        except GitError as error:
+            raise CommandError("Git says: {}".format(error))
+        for key, targets in targets:
             for app, migration in targets:
-                management.call_command("migrate", app, migration, fake=options["fake"], verbosity=options["verbosity"])
+                if options["dry_run"] and options["verbosity"] >= 1:
+                    self.stdout.write(self.style.MIGRATE_LABEL("{}.{}".format(app, migration)))
+                else:
+                    management.call_command(
+                        "migrate", app, migration, fake=options["fake"], verbosity=options["verbosity"]
+                    )
