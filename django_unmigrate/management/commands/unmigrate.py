266f7a94a999a18546814945205996b951cc554c
Lorenzo Peña <lorinkoz@gmail.com>
2020-04-29 01:11:45 -0400

    Added protection to unmigrate command when not in debug mode

--- django_unmigrate/management/commands/unmigrate.py
+++ django_unmigrate/management/commands/unmigrate.py
@@ -1,5 +1,6 @@
+from django.conf import settings
 from django.core import management
-from django.core.management.base import BaseCommand
+from django.core.management.base import BaseCommand, CommandError
 from django.db import DEFAULT_DB_ALIAS
 
 from django_unmigrate.core import get_targets
@@ -20,8 +21,13 @@ class Command(BaseCommand):
         parser.add_argument(
             "--fake", action="store_true", help="Mark migrations as run without actually running them.",
         )
+        parser.add_argument(
+            "--kamikazee", action="store_true", help="Ignore DEBUG=True and run the command anyways.",
+        )
 
     def handle(self, *args, **options):
+        if not settings.DEBUG and not options["kamikazee"]:
+            raise CommandError("Do not run with DEBUG=True, or pass --kamikazee.")
         for key, targets in get_targets(options["database"], options["branch"]).items():
             for app, migration in targets:
                 management.call_command("migrate", app, migration, fake=options["fake"], verbosity=options["verbosity"])
