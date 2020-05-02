34e0765cb83905cd53813904941cac32d47f882a
Lorenzo Peña <lorinkoz@gmail.com>
2020-05-01 21:44:53 -0400

    Added third and parallel test migration (repo checkpoint)

--- dunm_sandbox/myapp/models.py
+++ dunm_sandbox/myapp/models.py
@@ -5,3 +5,5 @@ class MyModel(models.Model):
 
     name = models.CharField(max_length=30, blank=True)
     tz_created = models.DateTimeField(auto_now_add=True)
+
+    is_paid = models.BooleanField(default=False)
