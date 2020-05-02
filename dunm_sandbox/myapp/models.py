3d0e74a401521511ab4e58f767661a4bbfedf32a
Lorenzo Peña <lorinkoz@gmail.com>
2020-05-01 21:49:27 -0400

    Added last test migration (repo checkpoint)

--- dunm_sandbox/myapp/models.py
+++ dunm_sandbox/myapp/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class MyModel(models.Model):
 
-    name = models.CharField(max_length=30, blank=True)
+    name = models.CharField(max_length=30)
     tz_created = models.DateTimeField(auto_now_add=True)
 
     is_paid = models.BooleanField(default=False)
